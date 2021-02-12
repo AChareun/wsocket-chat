913957a56331bf0c096c8a7e31041b407812a077
AChareun <achareun22@gmail.com>
2021-02-12 17:40:03 -0300

    add gulpfile to automate project compiling

---
+++ gulpfile.js
@@ -0,0 +1,25 @@
+const { src, dest, series } = require('gulp');
+const clean = require('gulp-clean');
+const ts = require('gulp-typescript');
+
+function cleanDist() {
+    return src(['./dist'], { read: false })
+        .pipe(clean())
+}
+
+function copyHbs() {
+    return src(['./src/**/*.hbs'])
+        .pipe(dest('./dist'));
+}
+
+function compileTs() {
+    const tsProject = ts.createProject('tsconfig.json');
+    const tsResult = tsProject.src()
+        .pipe(tsProject());
+
+    return tsResult.js.pipe(dest('dist'));
+}
+
+const compileTask = series([cleanDist, compileTs, copyHbs]);
+
+exports.default = compileTask;
