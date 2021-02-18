bb4858d7bd60d2c4972926263daecfcf2a0db989
AChareun <achareun22@gmail.com>
2021-02-18 16:16:35 -0300

    add new typings for User

--- src/interface.d.ts
+++ src/interface.d.ts
@@ -1,11 +1,24 @@
 import { Document } from "mongoose";
 
-export interface IUser extends Document {
+export interface IUserModel extends Document {
+    username: string,
+    password: string,
+}
+
+export interface IUserModelCreationAttributes {
+    id?: IUserModel['_id'],
+    username: IUserModel['username'],
+    password: IUserModel['password'],
+}
+
+export interface IUser {
+    id?: number,
     username: string,
     password: string,
 }
 
 export interface IUserCreationAttributes {
+    id?: IUser['id'],
     username: IUser['username'],
     password: IUser['password'],
 }
