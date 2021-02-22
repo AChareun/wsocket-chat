3cf3461121892421a949ba1d2905bfdab5fda442
AChareun <achareun22@gmail.com>
2021-02-22 14:45:31 -0300

    implement userController

---
+++ src/module/user/controller/userController.ts
@@ -0,0 +1,78 @@
+import { Application, Request, Response } from 'express';
+import { UserService } from '../service/userService';
+import { IUser } from '../../../interface';
+
+export class UserController {
+    private BASE_ROUTE = '/user';
+    private VIEWS_DIR = '../user/views';
+    private userService: UserService;
+
+    constructor(userService: UserService) {
+        this.userService = userService;
+    }
+
+    configureRoutes(app: Application) {
+        const ROUTE = this.BASE_ROUTE;
+
+        app.get(`${ROUTE}/:id`, this.getById.bind(this));
+        app.post(`${ROUTE}`, this.createUser.bind(this));
+        app.put(`${ROUTE}/:id`, this.updateUser.bind(this));
+        app.delete(`${ROUTE}/:id`, this.deleteUser.bind(this));
+    }
+
+    async createUser(req: Request, res: Response): Promise<void> {
+        const userData = req.body;
+        let newUser: IUser;
+        try {
+            newUser = await this.userService.addUser(userData);
+            res.status(200).json(newUser);
+        } catch (e) {
+            res.status(400).json(e);
+        }
+    }
+
+    async getById(req: Request, res: Response): Promise<void> {
+        const userId = req.params.id;
+        let requestedUser: IUser | undefined;
+        if (userId && !isNaN(parseInt(userId))) {
+            try {
+                requestedUser = await this.userService.getById(parseInt(userId));
+                res.status(200).json(requestedUser);
+            } catch (e) {
+                res.status(400).json(e);
+            }
+        } else {
+            res.status(500).send();
+        }
+    }
+
+    async updateUser(req: Request, res: Response): Promise<void> {
+        const userId = req.params.id;
+        const userData = req.body;
+        let updatedUser: IUser | undefined;
+        if (userData && userId && !isNaN(parseInt(userId))) {
+            try {
+                updatedUser = await this.userService.updateUser(parseInt(userId), userData);
+                res.status(200).json(updatedUser);
+            } catch (e) {
+                res.status(400).json(e);
+            }
+        } else {
+            res.status(500).send();
+        }
+    }
+
+    async deleteUser(req: Request, res: Response): Promise<void> {
+        const userId = req.params.id;
+        if (!isNaN(parseInt(userId))) {
+            try {
+                const isSuccess = await this.userService.deleteUser(parseInt(userId));
+                res.status(isSuccess ? 200 : 400).json(isSuccess);
+            } catch (e) {
+                res.status(400).json(e);
+            }
+        } else {
+            res.status(500).send();
+        }
+    }
+}
