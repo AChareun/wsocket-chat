b094da2034e7131bb6e65887b74af1dda1fcca62
AChareun <achareun22@gmail.com>
2021-02-22 14:44:45 -0300

    fix User and IUser prop conflict

--- src/module/user/entity/user.ts
+++ src/module/user/entity/user.ts
@@ -1,34 +1,13 @@
 import { IUser, IUserCreationAttributes } from '../../../interface';
 
 export class User implements IUser {
-    private _id?: number;
-    private _password: string;
-    private _username: string;
+    id?: number;
+    password: string;
+    username: string;
 
     constructor(attributes: IUserCreationAttributes) {
-        this._id = attributes.id;
-        this._username = attributes.username;
-        this._password = attributes.password;
-    }
-
-    get username(): string {
-        return this._username;
-    }
-    set username(value: string) {
-        this._username = value;
-    }
-
-    get password(): string {
-        return this._password;
-    }
-    set password(value: string) {
-        this._password = value;
-    }
-
-    get id(): number | undefined {
-        return this._id;
-    }
-    set id(value: number | undefined) {
-        this._id = value;
+        this.id = attributes.id;
+        this.username = attributes.username;
+        this.password = attributes.password;
     }
 }
