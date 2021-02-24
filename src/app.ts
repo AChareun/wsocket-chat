eccf05ee8d588829bb40b7a16eadeb6c80fb7bed
AChareun <achareun22@gmail.com>
2021-02-24 18:52:49 -0300

    implement session configs on app.ts

--- src/app.ts
+++ src/app.ts
@@ -4,13 +4,15 @@ import path = require('path');
 import cookieParser = require('cookie-parser');
 import logger = require('morgan');
 import sassMiddleware = require('node-sass-middleware');
-import {HttpError} from "http-errors";
-import {NextFunction, Request, Response} from "express";
+import { HttpError } from 'http-errors';
+import { NextFunction, Request, Response } from 'express';
+import * as session from 'express-session';
 
 import { configureDI } from './config/di';
 import { defaultModuleInit } from './module/default/module';
-import { userModuleInit } from "./module/user/module";
-import { configureMongoConnection } from "./config/db";
+import { userModuleInit } from './module/user/module';
+import { configureMongoConnection } from './config/db';
+import * as passport from 'passport';
 
 const app = express();
 
@@ -22,34 +24,49 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(sassMiddleware({
-  src: path.join(__dirname, 'public'),
-  dest: path.join(__dirname, 'public'),
-  indentedSyntax: true, // true = .sass and false = .scss
-  sourceMap: true
-}));
+app.use(
+    sassMiddleware({
+        src: path.join(__dirname, 'public'),
+        dest: path.join(__dirname, 'public'),
+        indentedSyntax: true, // true = .sass and false = .scss
+        sourceMap: true,
+    })
+);
 app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(
+    session({
+        secret: 'socket-cat',
+        resave: false,
+        saveUninitialized: true,
+    })
+);
 
 configureMongoConnection(process.env.DB_PATH || '');
 
 const diContainer = configureDI();
+
+const passportMiddleware = diContainer.get<typeof passport>('Passport');
+app.use(passportMiddleware.initialize());
+app.use(passportMiddleware.session());
+
 defaultModuleInit(app, diContainer);
 userModuleInit(app, diContainer);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
+app.use(function (req, res, next) {
+    next(createError(404));
 });
 
 // error handler
-app.use(function(err: HttpError, req: Request, res: Response, next: NextFunction) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+app.use(function (err: HttpError, req: Request, res: Response, next: NextFunction) {
+    // set locals, only providing error in development
+    res.locals.message = err.message;
+    res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
+    // render the error page
+    res.status(err.status || 500);
+    res.render('error');
 });
 
 export default app;
